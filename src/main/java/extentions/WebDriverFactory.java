b5545561b28de01009c14618e2251ec8e3d1e3c6
Maria Egorova <ab-ermakova@yandex.ru>
2023-03-04 15:12:22 +0100

    new commit

---
+++ src/main/java/extentions/WebDriverFactory.java
@@ -0,0 +1,38 @@
+package extentions;
+import config.AppConfig;
+import pages.HomePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+    private static ChromeDriver driver;
+
+
+    public static WebDriver get() {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/ermakova20/Downloads/WebDriver/bin/chromedriver_mac64/chromedriver");
+
+        String BrowserName = System.getenv().get("browser");
+
+        WebDriver driver;
+        switch (BrowserName) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                throw new RuntimeException("Browser " + BrowserName + " not exist");
+
+        }
+
+
+        driver.get(AppConfig.URL);
+        return driver;
+    }
+}
